054a8fd7564f9578c9b94d549c8f91d90c55f071
wq5451 <1253536422@users.noreply.github.com>
2020-01-19 17:24:36 +0800

    stage_test01

---
+++ app/src/main/java/com/example/myapplication/Main/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplication.Main;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends Activity {
+
+	private MySlideView msv;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		msv = (MySlideView) findViewById(R.id.msv);
+		
+		
+		findViewById(R.id.main_back).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				msv.changeState();
+				
+			}
+		});
+	}
+	
+	public void onClick(View v){
+		
+		TextView tv = (TextView) v;
+		Toast.makeText(this,tv.getText(), 0).show();
+	}
+
+
+}
